1eb04ed998f9365631ce48f23682e65def57a68d
Luiz Gustavo <luizgustavodsoares@gmail.com>
2024-11-11 16:31:51 -0300

    Fix: add ewuipe page, and modals

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,49 @@ document.addEventListener("DOMContentLoaded", function () {
         navbarMenu.classList.toggle("hidden");
     });
 });
+
+// main.js
+async function loadModal() {
+    const response = await fetch("./src/pages/utils/modal.html");
+    const modalContent = await response.text();
+    document.body.insertAdjacentHTML("beforeend", modalContent);
+    document.getElementById("modal").classList.remove("hidden");
+}
+
+function closeModal() {
+    document.getElementById("modal").classList.add("hidden");
+}
+
+// Carrossel
+document.addEventListener('DOMContentLoaded', function() {
+    const prevButton = document.querySelector('[data-carousel-prev]');
+    const nextButton = document.querySelector('[data-carousel-next]');
+    const items = document.querySelectorAll('[data-carousel-item]');
+    let currentIndex = 0;
+
+    // Função para mostrar o item atual e esconder os outros
+    function showItem(index) {
+        items.forEach((item, idx) => {
+            if (idx === index) {
+                item.classList.remove('hidden');
+            } else {
+                item.classList.add('hidden');
+            }
+        });
+    }
+
+    // Ação do botão anterior
+    prevButton.addEventListener('click', function() {
+        currentIndex = (currentIndex - 1 + items.length) % items.length;  // Desce o índice
+        showItem(currentIndex);
+    });
+
+    // Ação do botão próximo
+    nextButton.addEventListener('click', function() {
+        currentIndex = (currentIndex + 1) % items.length;  // Sobe o índice
+        showItem(currentIndex);
+    });
+
+    // Mostrar o primeiro item
+    showItem(currentIndex);
+});
