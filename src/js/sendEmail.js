3ad502a0a331f759d62cbff61d3fa009ea5aceb3
Luiz Gustavo <luizgustavodsoares@gmail.com>
2024-11-22 11:24:05 -0300

    Fix: Send email with Alert

--- src/js/sendEmail.js
+++ src/js/sendEmail.js
@@ -1,26 +1,58 @@
-// Inicializando o EmailJS com a sua chave pública
 emailjs.init("oFu7Pf0pZEL99iQgd");
 
-// Enviar o e-mail
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-    event.preventDefault();  // Evita o envio tradicional
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previne o envio tradicional do formulário
 
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var subject = document.getElementById("subject").value;
     var message = document.getElementById("message").value;
 
-    // Envia o e-mail
+    // Envio de e-mail usando EmailJS
     emailjs.send("service_6gcjtjt", "template_6n019ud", {
         name: name,
         email: email,
         subject: subject,
         message: message
-    }).then(function (response) {
-        alert("Mensagem enviada com sucesso!");
-        console.log("Sucesso:", response);
-    }).catch(function (error) {
-        alert("Erro ao enviar a mensagem. Tente novamente.");
+    }).then(function(response) {
+        showAlert("Mensagem enviada com sucesso!", "success");
+        console.log("Mensagem enviada:", response);
+    }).catch(function(error) {
+        showAlert("Erro ao enviar a mensagem. Tente novamente.", "error");
         console.log("Erro:", error);
     });
 });
+
+function showAlert(message, type) {
+    var alertBox = document.getElementById("custom-alert");
+    var alertMessage = document.getElementById("alert-message");
+    var closeButton = document.getElementById("close-alert");
+
+    // Atualiza o texto do alerta
+    alertMessage.innerText = message;
+
+    // Altera a cor do alerta com base no tipo
+    if (type === "success") {
+        alertBox.classList.remove("bg-red-500");
+        alertBox.classList.add("bg-green-500");
+        alertBox.classList.add("flex")
+    } else if (type === "error") {
+        alertBox.classList.remove("bg-green-500");
+        alertBox.classList.add("bg-red-500");
+        alertBox.classList.add("flex")
+    }
+
+    // Exibe o alerta
+    alertBox.classList.remove("hidden");
+
+    // Fecha o alerta quando o botão de fechar é clicado
+    closeButton.addEventListener("click", function() {
+        alertBox.classList.remove("flex")
+        alertBox.classList.add("hidden");
+    });
+
+    // Fecha o alerta após 5 segundos
+    setTimeout(function() {
+        alertBox.classList.add("hidden");
+    }, 5000);
+}
